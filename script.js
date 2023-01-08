5843d318485175f3fdc9025d8d1bfed9ebc15e90
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2023-01-08 20:10:14 +0530

    Hold button and win
    crate hold button function and winning event handling.

--- script.js
+++ script.js
@@ -21,26 +21,55 @@ diceEl.classList.add('hidden');
 const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  playerEl0.classList.toggle('player--active');
+  playerEl1.classList.toggle('player--active');
+};
 
 //rolling Dice functionality
 btnRoll.addEventListener('click', function () {
-  //create a random dice No.
-  const diceNo = Math.trunc(Math.random() * 6) + 1;
-  //display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${diceNo}.png`;
-  //if dice not 1 let continue if it 1 change player
-  if (diceNo !== 1) {
-    //add dice no to current score
-    currentScore += diceNo;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    //switch user
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    playerEl0.classList.toggle('player--active');
-    playerEl1.classList.toggle('player--active');
+  if (playing) {
+    //create a random dice No.
+    const diceNo = Math.trunc(Math.random() * 6) + 1;
+    //display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${diceNo}.png`;
+    //if dice not 1 let continue if it 1 change player
+    if (diceNo !== 1) {
+      //add dice no to current score
+      currentScore += diceNo;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //switch user
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //add current value to score
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    //if score equal or grater than 100 that player wins
+    if (score[activePlayer] >= 20) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      //change player
+      switchPlayer();
+    }
   }
 });
